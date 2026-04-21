19d791d562008e0a13b869e3fc4b036ab0ff33cd
Eric Schwartz <erics273@gmail.com>
2026-04-21 12:28:30 -0400

    try catch stuff

---
+++ src/main/java/com/pluralsight/BufferedWriterExample.java
@@ -0,0 +1,28 @@
+package com.pluralsight;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+public class BufferedWriterExample {
+
+    public static void main(String[] args) {
+
+        try {
+            // create a FileWriter //use appent set to true if you want to append to the file instead of overwrting the contents
+            FileWriter fileWriter = new FileWriter("src/main/resources/text.txt");
+            // create a BufferedWriter
+            BufferedWriter bufWriter = new BufferedWriter(fileWriter);
+            // write to the file
+            bufWriter.write("id|name|gross pay\n");
+            bufWriter.write("111|Cameron Tay|3277.65\n");
+            bufWriter.write("222|James Tee|2150.00\n");
+            // close the writer
+            bufWriter.close();
+        } catch (Exception e) {
+            System.out.println("ERROR: An unexpected error occurred");
+            e.getStackTrace();
+        }
+    }
+
+}
+
